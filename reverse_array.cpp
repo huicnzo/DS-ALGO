d52fff381fde8f80b8bf2c4bf070c35b7b60ddf7
mahiiiiemaaaa <73066217+huicnzo@users.noreply.github.com>
2021-03-24 16:47:15 +0530

    Create reverse_array.cpp

---
+++ reverse_array.cpp
@@ -0,0 +1,13 @@
+/* Recursive method */
+/* https://en.cppreference.com/w/cpp/language/return
+If control reaches the end of
+
+a function with the return type (possibly cv-qualified) void,
+a constructor,
+a destructor, or
+a function-try-block for a function with the return type (possibly cv-qualified) void
+without encountering a return statement, return; is executed.
+
+If control reaches the end of the main function, return 0; is executed. */
+
+
