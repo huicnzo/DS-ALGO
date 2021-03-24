b7ed59cbeca68e87c89445ed361d627aec56e087
mahiiiiemaaaa <73066217+huicnzo@users.noreply.github.com>
2021-03-24 16:49:14 +0530

    Update reverse_array.cpp

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -11,3 +11,55 @@ without encountering a return statement, return; is executed.
 If control reaches the end of the main function, return 0; is executed. */
 
 
+
+/* 1) Initialize start and end indexes as start = 0, end = n-1 
+2) Swap arr[start] with arr[end] 
+3) Recursively call reverse for rest of the array. */
+
+// Recursive C++ program to reverse an array
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to reverse arr[] from start to end*/
+void rvereseArray(int arr[], int start, int end)
+{
+	if (start >= end)
+	return;
+	
+	int temp = arr[start];
+	arr[start] = arr[end];
+	arr[end] = temp;
+	
+	// Recursive Function calling
+	rvereseArray(arr, start + 1, end - 1);
+}	
+
+
+/* Utility function to print an array */
+void printArray(int arr[], int size)
+{
+for (int i = 0; i < size; i++)
+cout << arr[i] << " ";
+
+cout << endl;
+}
+
+/* Driver function to test above functions */
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5, 6};
+	
+	// To print original array
+	printArray(arr, 6);
+	
+	// Function calling
+	rvereseArray(arr, 0, 5);
+	
+	cout << "Reversed array is" << endl;
+	
+	// To print the Reversed array
+	printArray(arr, 6);
+	
+	return 0;
+}
+
